4cab3fb1a855e852c177122617678a7ef3d501c3
Michelle Rosendal <Michelle@desktop-kh6hbkh.kea.local>
2018-12-11 13:39:53 +0100

    contact og footer

--- about.php
+++ about.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="da">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,12 +11,14 @@
 </head>
 
 <body>
-   <?php include "header.html"; ?>
+    <?php include "header.html"; ?>
 
 
 
 
-   <?php include "footer.html"; ?>
+
+    <?php include "footer.html"; ?>
     <script src=""></script>
 </body>
+
 </html>
--- contact.php
+++ contact.php
@@ -1,24 +1,50 @@
 <!DOCTYPE html>
 <html lang="da">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
     <link rel="stylesheet" href="samlet.css">
-    <link rel="stylesheet" href="contactStyle.css">
+
+
     <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
 </head>
 
 <body>
-   <?php include "header.html"; ?>
+    <?php include "header.html"; ?>
+
+
+    <section class="pagecontainer">
+        <template id="temp">
+            <h1 id="data-pagetitle">title</h1>
+            <article id="data-page"></article>
+        </template>
+    </section>
 
 
 
+    <?php include "footer.html"; ?>
+
+    <script>
+        async function getJson() {
+
+            let pagetemplate = document.querySelector("#temp");
+            let pagecontainer = document.querySelector(".pagecontainer");
 
 
+            let jsonObjekt = await fetch("http://schaadtdesign.dk/kea/eksamen/wordpress/wp-json/wp/v2/pages/27");
+            let page = await jsonObjekt.json();
 
-   <?php include "footer.html"; ?>
+            let klon = pagetemplate.cloneNode(true).content;
+            klon.querySelector("#data-pagetitle").textContent = page.title.rendered;
+            klon.querySelector("#data-page").innerHTML = page.content.rendered;
+            pagecontainer.appendChild(klon);
+        };
 
-    <script src=""></script>
+        document.addEventListener("DOMContentLoaded", getJson);
+
+    </script>
 </body>
+
 </html>
